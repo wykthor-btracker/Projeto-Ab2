1404123e0f29cd92ab12058dc7f14251c63210e5
ABuarque <abuarquemf@gmail.com>
2017-03-21 15:35:46 -0300

    added dependencie folder

---
+++ Cliente.h
@@ -0,0 +1,18 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include "dependencias/stringHandling.c"
+
+struct cliente {
+	char nome[101];
+	char endereco[201];
+	char telefone[12];
+	unsigned int codigoCliente;
+};
+
+typedef struct cliente* Cliente;
+
+Cliente novoCliente(String nome, String endereco, 
+	String telefone, unsigned int codigoCliente);
+
+#endif
