0dd539b8c53fc9289a429c99140a1a501ccc2186
ABuarque <abuarquemf@gmail.com>
2017-03-26 20:29:01 -0300

    finished Cliente implementation

--- Cliente.c
+++ Cliente.c
@@ -1,9 +1,54 @@
 #include "Cliente.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "dependencias/stringHandling.h"
 
 //TO GET OBJECT FILE (Cliente.o)
 //gcc -c Cliente.c
 
 Cliente novoCliente(String nome, String endereco, 
-	String telefone, unsigned int codigoCliente);
+	String telefone, unsigned int codigoCliente) {
+	Cliente cliente = (Cliente) malloc(sizeof(tamanhoCliente));
+	copyString(cliente->nome, nome);
+	copyString(cliente->endereco, endereco);
+	copyString(cliente->telefone, telefone);
+	cliente->codigoCliente = codigoCliente;
+	return cliente;
+}
+
+String pegarNome(Cliente cliente) {
+	return cliente->nome;
+}
+
+String pegarEndereco(Cliente cliente) {
+	return cliente->endereco;
+}
+
+String pegarTelefone(Cliente cliente) {
+	return cliente->telefone;
+}
+
+int pegarCodigoCliente(Cliente cliente) {
+	return cliente->codigoCliente;
+}
+
+void mudarNome(Cliente cliente, String novoNome) {
+	copyString(cliente->nome, novoNome);
+}
+
+void mudarEndereco(Cliente cliente, String novoEndereco) {
+	copyString(cliente->endereco, novoEndereco);
+}
+
+void mudarTelefone(Cliente cliente, String novoTelefone) {
+	copyString(cliente->telefone, novoTelefone);
+}
+
+void mudarCodigoCliente(Cliente cliente, 
+					unsigned int novoCodigo) {
+	cliente->codigoCliente = novoCodigo;
+}
+
+void destruirCliente(Cliente cliente) {
+	free(cliente);
+}
--- Cliente.h
+++ Cliente.h
@@ -10,9 +10,103 @@ struct cliente {
 	unsigned int codigoCliente;
 };
 
+/*
+ * Remove a necessidade de digitar struct cliente
+ * e trabalha diretamente com ponteiros para cada
+ * objeto cliente.
+ */
 typedef struct cliente* Cliente;
 
+/*
+ * Sera utilizado na criacao dinamica dos objetos
+ * clientes.
+ */
+typedef struct cliente tamanhoCliente;
+
+/*
+ * Essa funcao funciona como construtor
+ * do objeto cliente.
+ *
+ * @param nome
+ * @param endereco
+ * @param telefone
+ * @param codigo do cliente
+ * @return um objeto cliente
+ */
 Cliente novoCliente(String nome, String endereco, 
 	String telefone, unsigned int codigoCliente);
+
+/*
+ * Esse funcao retorna o nome do cliente.
+ *
+ * @param objeto cliente
+ * @return o nome do cliente
+ */
+String pegarNome(Cliente cliente);
+
+/*
+ * Esse funcao retorna o endereco do cliente.
+ *
+ * @param objeto cliente
+ * @return o endereco
+ */
+String pegarEndereco(Cliente cliente);
+
+/*
+ * Esse funcao retorna o telefone do cliente.
+ *
+ * @param objeto cliente
+ * @return o telefone do cliente
+ */
+String pegarTelefone(Cliente cliente);
+
+/*
+ * Esse funcao retorna o codigo do cliente.
+ *
+ * @param objeto cliente
+ * @return o codigo do cliente
+ */
+int pegarCodigoCliente(Cliente telefone);
+
+/*
+ * Essa funcao altera o nome do cliente.
+ *
+ * @param cliente que ira ser modificado.
+ * @param o novo nome
+ */
+void mudarNome(Cliente cliente, String novoNome);
+
+/*
+ * Essa funcao altera o endereco do cliente.
+ *
+ * @param cliente que ira ser modificado.
+ * @param o novo endereco
+ */
+void mudarEndereco(Cliente cliente, String novoEndereco);
+
+/*
+ * Essa funcao altera o telefone do cliente.
+ *
+ * @param cliente que ira ser modificado.
+ * @param o novo telefone
+ */
+void mudarTelefone(Cliente cliente, String novoTelefone);
+
+/*
+ * Essa funcao altera o codigo do cliente.
+ *
+ * @param cliente que ira ser modificado.
+ * @param o novo codigo
+ */
+void mudarCodigoCliente(Cliente cliente, unsigned int novoCodigo);
+
+/*
+ * Essa funcao funciona como um destrutor
+ * que ira destruir as referencias dos objetos 
+ * clientes criados.
+ *
+ * @param cliente que sera destruido.
+ */
+void destruirCliente(Cliente cliente);
 
 #endif
