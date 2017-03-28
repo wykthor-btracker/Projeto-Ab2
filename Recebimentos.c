b7642051c9fd932e3a06ca81edf4dbbe15660c06
Wykthor <wykthor.g@wgvale.com>
2017-03-27 23:47:32 -0300

    Saving for merge

--- Recebimentos.c
+++ Recebimentos.c
@@ -6,7 +6,16 @@
 int RecebimentosRestantes(Cliente cliente)
 {
 	//Recebimentos eh uma lista global que contem em cada elemento uma struct do tipo Recebimento.
-	
+	int i,restantes = 3;
+	for(i=0;i<2;i++)
+	{
+		if(Recebimentos[cliente->codigoCliente+i].flag == 1) restantes--;
+	}
+	return restantes;
+}
+void SubstituirRecebimento(Recebimento recebimento,unsigned int NumDoc)
+{
+	Recebimentos[NumDoc] = recebimento;
 }
 int main()
 {
