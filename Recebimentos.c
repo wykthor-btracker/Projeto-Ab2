476da2e9ab371f8fe614ccb4d9dd113f92e4ee09
Wykthor <wykthor.g@wgvale.com>
2017-03-28 08:19:35 -0300

    Mudar nome da listaRecebimentos

--- Recebimentos.c
+++ Recebimentos.c
@@ -3,7 +3,7 @@
 #include "Cliente.h"
 #include <stdio.h>
 //TODO
-Recebimento Recebimentos[30];//Solucao temporaria pra falta de uma lista global de structs
+Recebimento recebimentosLista[30];//Solucao temporaria pra falta de uma lista global de structs
 
 Recebimento copiarRecebimento(Recebimento antigoRecebimento,Recebimento novoRecebimento)
 {
@@ -18,8 +18,8 @@ int salvarRecebimento(Recebimento recebimento,Cliente cliente)
 {
 	 int id = gerarNumDoc(cliente);
 	 if(id == -1) return(0);
-	 Recebimentos[id] = copiarRecebimento(Recebimentos[id],recebimento);
-	 Recebimentos[id]->flag = 1;
+	 recebimentosLista[id] = copiarRecebimento(recebimentosLista[id],recebimento);
+	 recebimentosLista[id]->flag = 1;
  }
 int proximoRecebimento(Cliente cliente)
 {
@@ -29,7 +29,7 @@ int proximoRecebimento(Cliente cliente)
 	int i,pos = 0;
 	for(i=0;i<2;i++)
 	{
-		if(Recebimentos[cliente->codigoCliente+i]->flag == 1) pos++;
+		if(recebimentosLista[cliente->codigoCliente+i]->flag == 1) pos++;
 	}
 	return(pos);
 }
@@ -41,5 +41,5 @@ int gerarNumDoc(Cliente cliente)
 }
 Recebimento carregarRecebimento(unsigned int NumDoc)
 {
-	return(Recebimentos[NumDoc]);
+	return(recebimentosLista[NumDoc]);
 }
