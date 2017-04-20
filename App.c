64ab124050858c984063a56a539f084ffc270eb7
ABuarque <abuarquemf@gmail.com>
2017-04-20 20:05:07 -0300

    insert clients function ok

--- App.c
+++ App.c
@@ -11,7 +11,7 @@
 #include <locale.h>
 #include <ctype.h>
 
-char programaLidado = 's';
+#define DEBUG if(1)
 
 void cabecalho() {
 	printf("|---------------------------------------------------------------------------------------|\n");
@@ -19,17 +19,56 @@ void cabecalho() {
 	printf("|                                     GNU-GLP license                                   |\n");
 	printf("|                                   [version 1.0 - 2017]                                |\n"); 
 	printf("|---------------------------------------------------------------------------------------|\n");
-	/*const int SCREN_SIZE = 100;
-	int i;
-	printf("|");
-	for(i = 0; i < SCREN_SIZE; i++)
-		printf("-");
-	printf("|\n");
-	printf("|"); */
 }
 
 void inserirNovoCliente() {
-	printf("1\n");
+	int numeroClientes, i;
+	printf("Quantos clientes deseja adicionar? ");
+	scanf("%d", &numeroClientes);
+	getchar();
+	Cliente* clientes = (Cliente*) 
+			malloc(sizeof(Cliente) * numeroClientes); 
+	//alocando o espaco de cada obj cliente para depois usar
+	for(i = 0; i < numeroClientes; i++)
+		clientes[i] = (Cliente) malloc(sizeof(tamanhoCliente));
+
+	for(i = 0; i < numeroClientes; i++) {
+		printf("Dados do cliente: \n");
+		char nome[101];
+		printf("Nome >>> ");
+		scanf("%[^\n]", nome);
+		getchar();
+		char endereco[201];
+		printf("Endereço >>> ");
+		scanf("%[^\n]", endereco);
+		getchar();
+		char telefone[12];
+		printf("Telefone >>> ");
+		scanf("%[^\n]", telefone);
+		getchar();
+		unsigned int codigoCliente;
+		printf("Codigo >>> ");
+		scanf("%u", &codigoCliente);
+		getchar();
+		clientes[i] = novoCliente(nome, endereco, telefone, codigoCliente);
+		DEBUG {
+			printf("Dados: \n");
+			printf("nome: %s\n", nome);
+			printf("endereco: %s\n", endereco);
+			printf("telefone: %s\n", telefone);
+			printf("codigoCliente: %u\n", codigoCliente);
+		}
+		DEBUG {
+			printf("Estado cliente: \n");
+			printf("nome: %s\n", pegarNome(clientes[i]));
+			printf("endereco: %s\n", pegarEndereco(clientes[i]));
+			printf("telefone: %s\n", pegarTelefone(clientes[i]));
+			printf("codigoCliente: %u\n", pegarCodigoCliente(clientes[i]));
+		}
+	}
+	int gravacaoOk = salvarClients(clientes, numeroClientes);
+	for(i = 0; i < numeroClientes; i++)
+		destruirCliente(clientes[i]);
 }
 
 void inserirNovoRecebimento() {
@@ -44,44 +83,52 @@ void buscarRecebimentoPorData() {
 	printf("4\n");
 }
 
-void checkOpcao(const int* opcao) {
-	switch(*opcao) {
-		case 1 :
-			inserirNovoCliente();
-			break;
-		case 2 :
-			inserirNovoRecebimento();
-			break;
-		case 3 :
-			alterarCadastroDeCliente();
-			break;
-		case 4 :
-			buscarRecebimentoPorData();
-			break;
-		case 5 :
-			exit(1);
-			break;
-		default :
-			printf("Viado\n");
-			break;
-	}
-}
-
 void menuPrincipal() {
-	int opcao;
 	printf("Digite uma das opções: \n");
 	printf("	(1) Inserir novo cliente.\n");
 	printf("	(2) Inserir novo recebimento.\n");
 	printf("	(3) Alterar cadastro do cliente.\n");
 	printf("	(4) Buscar recebimentos por data.\n");
 	printf("	(5) Encerrar programa.\n");
-	scanf("%d", &opcao);
-	checkOpcao(&opcao);
 }
 
 int main() {
+	system("clear");
 	setlocale(LC_ALL, "Portuguese");
-	menuPrincipal();
-
+	char desligar = 'n';
+	char opcao;
+	while(desligar == 'n' || desligar == 'N') {
+		cabecalho();
+		menuPrincipal();
+		printf(">>> ");
+		scanf("%c", &opcao);
+		DEBUG printf("***Opcao dada: %c\n", opcao);
+		getchar();
+		switch(opcao) {
+			case '1' :
+				inserirNovoCliente();
+				break;
+			case '2' :
+				inserirNovoRecebimento();
+				break;
+			case '3' :
+				alterarCadastroDeCliente();
+				break;
+			case '4' :
+				buscarRecebimentoPorData();
+				break;
+			case '5' :
+				exit(1);
+				break;
+			default :
+				printf("Opção inválida.\n");
+				break;
+		}
+		printf("Deseja fechar o programa (S/N)? ");
+		scanf("%c", &desligar);
+		getchar();
+		DEBUG printf("***desligar? %c\n", desligar);
+		system("clear");
+	}
 	return 0;
 }
