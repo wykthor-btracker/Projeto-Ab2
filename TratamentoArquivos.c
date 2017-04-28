7332388ee6ea3e8621edf8883a98c9ccfe9ff4c2
ABuarque <abuarquemf@gmail.com>
2017-04-28 01:35:02 -0300

    finished gravarRecebimentos function

--- TratamentoArquivos.c
+++ TratamentoArquivos.c
@@ -1,10 +1,11 @@
-#include "TratamentoArquivos.c"
+#include "TratamentoArquivos.h"
 #include "Recebimentos.h"
 #include "dependencias/stringHandling.h"
+#include "Cliente.h"
 #include <stdio.h>
 #include <stdlib.h>
 
-const char CAMNHO[] = "recebimentos.dat"
+const char CAMNHO[] = "recebimentos.dat";
 
 /*
 tamanho index
@@ -16,16 +17,36 @@ token separador $?
 
 */
 void gravarRecebimentos(Recebimentos* lista) {
-	int i;
+	int i, j;
 	FILE* escritor = fopen(CAMNHO, "w");
+	//gravando tamanho e posicao do index da lista
+	fprintf(escritor, "%d %d\n", lista->tamanho, lista->index);
 	for(i = 0; i < lista->index; i++) {
-
+		Cliente cli = lista->nodes[i]->cliente;
+		//gravando os dados do cliente
+		fprintf(escritor, "%s$?%d$?%s$?%s$?", 
+			cli->nome, cli->codigoCliente, cli->endereco, cli->telefone);
+		//gravando dados dos recebimentos do cliente
+		int recebFeitos = lista->nodes[i]->recebimentosFeitos;
+		if(recebFeitos == 0)
+			fprintf(escritor, "\n");
+		else {
+			for(j = 0; j < recebFeitos; j++) {
+				Recebimento r = lista->nodes[i]->rec[j];
+				fprintf(escritor, "%d$?%.2f$?%d$?%d$?%d$?%d$?%d$?%d$?%d$?%d\n",
+					r->numeroDocumento, r->valorRecebimento,
+					r->dataEmissao.dia, r->dataEmissao.mes,
+					r->dataEmissao.ano, r->dataVencimento.dia, 
+					r->dataVencimento.mes, r->dataVencimento.ano, 
+					r->codigoCliente, r->flag);
+			}
+		}
 	}
 	fclose(escritor);
 }
 
 Recebimentos* bufferizarRecebimentos() {
-	Recebimentos buffer;
+	Recebimentos* buffer = NULL;
 	//.....
 	return buffer;
 }
